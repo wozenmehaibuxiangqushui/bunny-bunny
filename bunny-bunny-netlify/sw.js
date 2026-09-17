c2769bb8b08af3a0f290a071ae96db30d955eed0
wozenmehaibuxiangqushui <77827343+wozenmehaibuxiangqushui@users.noreply.github.com>
2026-09-17 23:46:28 +0800

    Fix legacy widget cascade and cap wide-screen surface proportions

--- bunny-bunny-netlify/sw.js
+++ bunny-bunny-netlify/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "bunny-bunny-shell-v31";
+const CACHE_NAME = "bunny-bunny-shell-v32";
 const OFFLINE_URL = new URL("./index.html", self.registration.scope).href;
 const APP_SHELL = [
   './css/apps/widget-surfaces.css', './js/apps/widget-surfaces.js',
